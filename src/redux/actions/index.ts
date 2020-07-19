7faf6fb8891d6d3ce8e51d3d87800b57b74c724b
Yang Jae Lee <remd@naver.com>
2020-07-19 23:26:05 +0900

    07/19 Redux 관련 삭제

--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,18 +1,8 @@
 import addArticle from './article';
 import { loginStatusChange, logout } from './loginCheck';
-import { createNewBoard, cancelCreatingBoard, submitNewBoard } from './simple_trello/BoardActions';
-import { archiveCard, handleDrop, submitNewCard, submitList, enableListEditMode } from './simple_trello/CardActions';
 
 export {
 	addArticle,
 	loginStatusChange,
 	logout,
-	createNewBoard,
-	submitNewBoard,
-	cancelCreatingBoard,
-	archiveCard,
-	handleDrop,
-	submitNewCard,
-	submitList,
-	enableListEditMode
 };
